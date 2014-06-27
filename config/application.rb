b2b44125d4c4fcd85a455c698afd1562bc896ee8
minifigmaster125 <xchaoscontrolx@gmail.com>
2014-06-27 08:59:25 -0700

    Still wokring with heroku

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,7 @@ module CRUDlist
 
     # Heroku requires this to be false
     config.assets.initialize_on_precompile=false
+    config.serve_static_assets = true
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
