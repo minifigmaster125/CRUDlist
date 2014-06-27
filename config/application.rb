5ab302890bf34083e16e81208f96e0029b0e2ad5
minifigmaster125 <xchaoscontrolx@gmail.com>
2014-06-27 08:55:03 -0700

    Trying to configure assets for heroku deploy.

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,10 @@ module CRUDlist
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
+    #
+
+    # Heroku requires this to be false
+    config.assets.initialize_on_precompile=false
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
