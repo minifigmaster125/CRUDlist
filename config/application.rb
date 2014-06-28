c8f70082ee43ae28f39e6385f259959a3fd03690
minifigmaster125 <xchaoscontrolx@gmail.com>
2014-06-28 12:18:09 -0700

    still trying dynamic assets heroku

--- config/application.rb
+++ config/application.rb
@@ -17,9 +17,6 @@ module CRUDlist
     # config.time_zone = 'Central Time (US & Canada)'
     #
 
-    # Heroku requires this to be false
-    config.assets.initialize_on_precompile=false
-    config.serve_static_assets = true
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
