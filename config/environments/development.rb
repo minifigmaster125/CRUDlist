28d8f28791bb092af0b13daff7bed2c6fa67ad79
minifigmaster125 <xchaoscontrolx@gmail.com>
2014-06-28 13:43:39 -0700

    still trying dynamic assets heroku

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,7 +6,6 @@ CRUDlist::Application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
-  config.assets.digest = true
 
   # Do not eager load code on boot.
   config.eager_load = false
